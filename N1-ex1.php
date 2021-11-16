f28eb6f5540a63c6442c934eea7e939a05103cca
Xavier Bonilla <xavier.bonilla.moliner@gmail.com>
2021-11-16 12:52:26 +0100

    Versió 1.0. Inclou Nivell 1-Exercici 1

---
+++ N1-ex1.php
@@ -0,0 +1,16 @@
+<?php
+
+    function numeroparoimpar($numero){
+        if ($numero % 2 == 0){
+            echo "L'edat es par";
+        }
+        else {
+           echo "L'edat es impar";     
+        }
+    }
+
+    numeroparoimpar(15);
+
+
+
+?>
